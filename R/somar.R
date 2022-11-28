24d5fad4b4ad6421c4ae32d74d8e1d8284a81d5d
arethasilvaa <aretha.silva@ufba.br>
2022-11-28 12:55:23 -0300

    first commit

---
+++ R/somar.R
@@ -0,0 +1,17 @@
+#' Função para somar
+#'
+#' Esta função serve para fazer a soma de dois números
+#'
+#' @param a um número
+#' @param b outro número
+#'
+#' @examples
+#' somar(3, 4)
+#'
+#' @export
+
+somar = function(a, b){
+  a + b
+}
+
+
---
+++ R/subtrair.R
@@ -0,0 +1,16 @@
+#' Função para subtrair
+#'
+#' Esta função serve para fazer a subtração de dois números
+#'
+#' @param a um número
+#' @param b outro número
+#'
+#' @examples
+#' subtrair(3, 4)
+#'
+#' @export
+
+
+subtrair = function(a, b){
+  a - b
+}
